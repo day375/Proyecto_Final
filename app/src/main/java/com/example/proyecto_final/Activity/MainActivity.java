f2ce6ef3ff7f4e5b14902bbf3e4722791ef9cd9d
day375 <dajoraz375@gmail.com>
2024-06-19 17:21:08 -0500

    fin de segundo dia

--- app/src/main/java/com/example/proyecto_final/Activity/MainActivity.java
+++ app/src/main/java/com/example/proyecto_final/Activity/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.proyecto_final.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.CompositePageTransformer;
 import androidx.viewpager2.widget.MarginPageTransformer;
 
+import com.example.proyecto_final.Adapter.CategoryAdapter;
 import com.example.proyecto_final.Adapter.SliderAdapter;
+import com.example.proyecto_final.Domain.Category;
 import com.example.proyecto_final.Domain.Location;
 import com.example.proyecto_final.Domain.SliderItems;
 import com.example.proyecto_final.R;
@@ -31,7 +35,35 @@ public class MainActivity extends BaseActivity {
 
         initLocation();
         initBanner();
+        initCategory();
 
+    }
+
+    private void initCategory() {
+        DatabaseReference myRef=database.getReference("Category");
+        binding.progressBarCategory.setVisibility(View.VISIBLE);
+        ArrayList<Category> list=new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot issue:snapshot.getChildren()){
+                        list.add(issue.getValue(Category.class));
+                    }
+                    if (!list.isEmpty()){
+                        binding.recyclerViewCategory.setLayoutManager(new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false));
+                        RecyclerView.Adapter adapter=new CategoryAdapter(list);
+                        binding.recyclerViewCategory.setAdapter(adapter);
+                    }
+                    binding.progressBarCategory.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void initLocation() {
@@ -71,7 +103,7 @@ public class MainActivity extends BaseActivity {
 
     private void initBanner(){
         DatabaseReference myRef= database.getReference("Banner");
-        binding.progressBarBanner.setVisibility(RecyclerView.VISIBLE);
+        binding.progressBarBanner.setVisibility(View.VISIBLE);
         ArrayList<SliderItems> items=new ArrayList<>();
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -81,7 +113,7 @@ public class MainActivity extends BaseActivity {
                         items.add(issue.getValue(SliderItems.class));
                     }
                     banners(items);
-                    binding.progressBarBanner.setVisibility(RecyclerView.GONE);
+                    binding.progressBarBanner.setVisibility(View.GONE);
                 }
             }
 
